947cf56d3ad9628c8a89d7d2ec4f10c0bc4093bf
Bitopan Das <bitopan21mbb@gmail.com>
2026-07-22 18:06:30 +0530

    updated folder structure and air config

---
+++ cmd/web/handlers.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	// w.Write([]byte("Hello from Snippetbox"))
+
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	sId := r.URL.Query().Get("id")
+	iId, err := strconv.Atoi(sId)
+	if err != nil || iId < 1 {
+		http.Error(w, "Invalid ID", http.StatusNotFound)
+		return
+	}
+	w.Write([]byte("Viewing a specific snippet: " + sId))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost) /*setting the header should be done before
+		WriteHeader/Write is called, otherwise no effect*/
+
+		w.Header().Add("Cache-Control", "public")
+		w.Header().Add("Cache-Control", "max-age=31536000")
+
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed) /*we can use this helper function
+		which calls WriteHeader and Write behind the scenes*/
+		return
+	}
+	w.Write([]byte("Creating a new snippet..."))
+}
---
+++ cmd/web/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux() /*although a DefaultServeMux is provided by Go, we will create our
+	own mux to have more control over the routing of our application
+	and also for the sake of security*/
+
+	//routes
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("Server Started on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
